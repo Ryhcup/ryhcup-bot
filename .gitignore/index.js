9b5175497b1e76a2407f8ef0c115ef82490da790
Ryhcup <40119216+Ryhcup@users.noreply.github.com>
2018-06-09 15:40:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,11 +6,11 @@ var prefix = ("!")
 bot.on('ready', function () {
   console.log(`Je suis connecté en tant que ${bot.user.tag}!`); // Dis à la console qu'il viens de se connecter et en tant que qui
   bot.user.setActivity('Administrer la Cup')
-})
+});
 
 bot.on('guildCreate', guild => {
   console.log(`Je viens de rejoinde ${guild.name}`); // Dis à la console que le bot viens de rejoindre tel serveur
-})
+});
 
 bot.on('message', message => {
   if (message.content === prefix + 'ping') {
@@ -28,13 +28,11 @@ bot.on('message', message => {
   if (message.content === prefix + 'guide') {
     message.channel.send(`Pour lire le guide du tournoi: https://drive.google.com/open?id=1j8uw6fENKFj3GrEaC2P3YUX1vnNM0xe0`)
   }
-})
+});
 
 bot.on('guildMemberAdd', member => {
   console.log('User ' + member.displayName + ' as rejoint le serveur ROC'); // Dit à la console qu'un membre à rejoint
   member.guild.channels.get("435833840800628737").send(`Bienvenue sur le serveur de la RYH Overwatch Cup ${member} nous te conseillons vivement de lire le règlement et d'aller dans le channel #informations pour te renseigner sur la compétition`); // Envoie le message de bienvenue
   var role = member.guild.roles.find('name', 'Prout');
   member.addRole(role)
-})
-
-
+});
